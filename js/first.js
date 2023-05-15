5c88e283e7ac7cb6fd9dca502f205afa5e477167
Matias Tamburo <matitamburo68@gmail.com>
2023-05-15 09:54:19 -0300

    i primer commmit

---
+++ js/first.js
@@ -0,0 +1,21 @@
+//CONSTRUCTORESSS
+
+
+function msjAlert(contenido,posicion){
+    this.texto=contenido;
+    this.posicion=ubicacion;
+}
+
+var etiqueta = document.createElement("span");
+var mensaje = document.createTextNode(`El dato" "${texto}"  "no es valido`);
+
+
+
+
+
+etiqueta.appendChild(mensaje);
+
+document.getElementsById(posicion).appendChild(etiqueta)
+
+
+const alertamail = new msjAlert(getElementById());
